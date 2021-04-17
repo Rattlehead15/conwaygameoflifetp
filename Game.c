4361945088e6918f4c7977f37e646b1c8c08314a
Giuliano Regolo <Giuliano Regolo>
2021-04-17 19:01:09 -0300

    Agrego la lectura de archivos en game.c

---
+++ Game.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "Game.h"
+
+#define SIZE 300
+
+game_t *loadGame(const char *filename) {
+    FILE * entrada = fopen(filename, "r");
+    char str[SIZE];
+    int procesos, col, row;
+    fscanf(entrada,"%d %d %d %[^\0]", procesos, row, col, str);
+    game_t jueguito;
+    board_init(&jueguito.tablero, col, row);
+    board_load(&jueguito.tablero, str);
+}
+
+void writeBoard(board_t board, const char *filename) {
+    char str[300];
+    FILE * salida = fopen(salida,"w");
+    board_show(board,str);
+    fprintf(salida,"%s", str);
+}
+
+board_t *conwayGoL(board_t *board, unsigned int cycles, const int nuproc) {
+    board_t * nuevoTablero;
+
+    return nuevoTablero;
+}
+
+int main() {
+    return 0;
+}
